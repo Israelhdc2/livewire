cccd3949b6b979790d41827750e79b03ab51c7a8
Israelhdc2 <israelhdc2@gmail.com>
2022-01-10 05:04:40 -0500

    msg cuando no haya registro tabla

--- resources/views/livewire/show-posts.blade.php
+++ resources/views/livewire/show-posts.blade.php
@@ -16,51 +16,58 @@
                 <x-jet-input type="text" placeholder="Escriba lo que quiera buscar" class="w-full" wire:model="search" />
             </div>
 
-            <table class="min-w-full divide-y divide-gray-200">
-                <thead class="bg-gray-50">
-                  <tr>
-                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                      ID
-                    </th>
-                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                      Title
-                    </th>
-                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                      Content
-                    </th>
-                    <th scope="col" class="relative px-6 py-3">
-                      <span class="sr-only">Edit</span>
-                    </th>
-                  </tr>
-                </thead>
-                <tbody class="bg-white divide-y divide-gray-200">
+            @if ($posts->count())
+                <table class="min-w-full divide-y divide-gray-200">
+                    <thead class="bg-gray-50">
+                    <tr>
+                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        ID
+                        </th>
+                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        Title
+                        </th>
+                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        Content
+                        </th>
+                        <th scope="col" class="relative px-6 py-3">
+                        <span class="sr-only">Edit</span>
+                        </th>
+                    </tr>
+                    </thead>
+                    <tbody class="bg-white divide-y divide-gray-200">
 
-                    @foreach ($posts as $post)
-                        
-                        <tr>
-                            <td class="px-6 py-4 ">
-                                <div class="text-sm text-gray-900">
-                                    {{$post->id}}
-                                </div>
-                            </td>
-                            <td class="px-6 py-4 ">
-                                <div class="text-sm text-gray-900">
-                                    {{$post->title}}
-                                </div>
-                            </td>
-                            <td class="px-6 py-4 ">
-                                <div class="text-sm text-gray-900">
-                                    {{$post->content}}
-                                </div>
-                            </td>
-                            <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
-                            </td>
-                        </tr>
-      
-                    @endforeach
-                </tbody>
-              </table>
+                        @foreach ($posts as $post)
+                            
+                            <tr>
+                                <td class="px-6 py-4 ">
+                                    <div class="text-sm text-gray-900">
+                                        {{$post->id}}
+                                    </div>
+                                </td>
+                                <td class="px-6 py-4 ">
+                                    <div class="text-sm text-gray-900">
+                                        {{$post->title}}
+                                    </div>
+                                </td>
+                                <td class="px-6 py-4 ">
+                                    <div class="text-sm text-gray-900">
+                                        {{$post->content}}
+                                    </div>
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                    <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+                                </td>
+                            </tr>
+        
+                        @endforeach
+                    </tbody>
+                </table>
+            @else
+                <div class="px-6 py-4">
+                    No existe ningun registro coincidente
+                </div>                
+            @endif
+
         </x-table>
     </div>
 
