9b633a3c7d43ba42c2bb8c34755b360d31ba5bd6
Israelhdc2 <israelhdc2@gmail.com>
2022-01-20 04:08:08 -0500

    crear y eliminr carpet posts y exec las migrations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        
+        Storage::deleteDirectory('posts');
+        Storage::makeDirectory('posts');
         // \App\Models\User::factory(10)->create();
         Post::factory(100)->create();
 
