34839f6dc8fcb2ef2648b452fed6f71c51e2d7a3
Israelhdc2 <israelhdc2@gmail.com>
2022-01-24 00:35:03 -0500

    quitar etiquetas html d ckeditor

--- resources/views/livewire/show-posts.blade.php
+++ resources/views/livewire/show-posts.blade.php
@@ -92,7 +92,7 @@
                                 </td>
                                 <td class="px-6 py-4 ">
                                     <div class="text-sm text-gray-900">
-                                        {{$item->content}}
+                                        {!! $item->content !!}                                        
                                     </div>
                                 </td>
                                 <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">                                    
